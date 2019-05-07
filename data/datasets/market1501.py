1caf204702ff4a471c0c7e3184f1938f301a6620
Lucas Florin <lucas.florin@iosb.fraunhofer.de>
2019-05-07 18:33:38 +0200

    imported new market1501.py

--- data/datasets/market1501.py
+++ data/datasets/market1501.py
@@ -1,12 +1,35 @@
+# encoding: utf-8
+"""
+@author:  sherlock
+@contact: sherlockliao01@gmail.com
+"""
+
 import glob
-import os.path as osp
 import re
+
 import numpy as np
+import os.path as osp
 
-from .base import BaseDataset
+from utils.matlab_helper import MatlabMatrix
+from .bases import BaseImageDataset
 
 
-class Market1501(BaseDataset):
+def binarize(labels):
+    max_values = np.max(labels, axis=0)
+    bin_labels = np.zeros([len(labels), 0])
+    for i, v in enumerate(max_values):
+        if v > 2:
+            # binarize attribute if more than two values
+            loc = np.zeros([len(labels), v], dtype=int)
+            loc[(np.arange(len(labels)), labels[:, i] - 1)] = 1
+
+        else:
+            loc = (labels[:, i] - 1)[:, None]
+        bin_labels = np.concatenate([bin_labels, loc], axis=1)
+    return bin_labels.astype(int)
+
+
+class Market1501(BaseImageDataset):
     """
     Market1501
     Reference:
@@ -17,55 +40,43 @@ class Market1501(BaseDataset):
     # identities: 1501 (+1 for background)
     # images: 12936 (train) + 3368 (query) + 15913 (gallery)
     """
-    dataset_dir = 'market'
-    split_dir = 'data/datasets/splits'
+    dataset_dir = 'market1501'
 
-    def __init__(self, root='data', verbose=True, market1501_500k=False, **kwargs):
-        super(Market1501, self).__init__(root)
-        self.dataset_dir = osp.join(self.root, self.dataset_dir)
+    def __init__(self, root='/home/haoluo/data', verbose=True, **kwargs):
+        super(Market1501, self).__init__()
+        self.dataset_dir = osp.join(root, self.dataset_dir)
         self.train_dir = osp.join(self.dataset_dir, 'bounding_box_train')
         self.query_dir = osp.join(self.dataset_dir, 'query')
         self.gallery_dir = osp.join(self.dataset_dir, 'bounding_box_test')
-        self.extra_gallery_dir = osp.join(self.dataset_dir, 'images')
-        self.market1501_500k = market1501_500k
-        self.train_label_file = osp.join(self.split_dir, 'market_train.csv')
-        self.val_label_file = osp.join(self.split_dir, 'market_val.csv')
-        self.test_label_file = osp.join(self.split_dir, 'market_test.csv')
 
         self._check_before_run()
 
-        train, train_atts = self.load_from_file(self.train_label_file)
-        val,val_atts = self.load_from_file(self.val_label_file)
-        test, test_atts = self.load_from_file(self.test_label_file)
-        if len(train_atts) != len(val_atts) != len(test_atts):
-            raise Exception('Number of attributes doesn\'t match between dataset splits.')
-
-        self.attributes = train_atts
-        self.train = train
-        self.val = val
-        self.test = test
-
-        self.num_attributes = len(self.attributes)
-        self.train_imgs = len(train)
-        self.val_imgs = len(val)
-        self.test_imgs = len(test)
+        train = self._process_dir(self.train_dir, relabel=True)
+        query = self._process_dir(self.query_dir, relabel=False)
+        gallery = self._process_dir(self.gallery_dir, relabel=False)
 
         if verbose:
-            print('=> Market1501 loaded')
-            self.print_dataset_statistics(train, val, test, train_atts)
+            print("=> Market1501 loaded")
+            self.print_dataset_statistics(train, query, gallery)
+
+        self.train = train
+        self.query = query
+        self.gallery = gallery
+
+        self.num_train_pids, self.num_train_imgs, self.num_train_cams = self.get_imagedata_info(self.train)
+        self.num_query_pids, self.num_query_imgs, self.num_query_cams = self.get_imagedata_info(self.query)
+        self.num_gallery_pids, self.num_gallery_imgs, self.num_gallery_cams = self.get_imagedata_info(self.gallery)
 
     def _check_before_run(self):
         """Check if all files are available before going deeper"""
         if not osp.exists(self.dataset_dir):
-            raise RuntimeError('"{}" is not available'.format(self.dataset_dir))
+            raise RuntimeError("'{}' is not available".format(self.dataset_dir))
         if not osp.exists(self.train_dir):
-            raise RuntimeError('"{}" is not available'.format(self.train_dir))
+            raise RuntimeError("'{}' is not available".format(self.train_dir))
         if not osp.exists(self.query_dir):
-            raise RuntimeError('"{}" is not available'.format(self.query_dir))
+            raise RuntimeError("'{}' is not available".format(self.query_dir))
         if not osp.exists(self.gallery_dir):
-            raise RuntimeError('"{}" is not available'.format(self.gallery_dir))
-        if self.market1501_500k and not osp.exists(self.extra_gallery_dir):
-            raise RuntimeError('"{}" is not available'.format(self.extra_gallery_dir))
+            raise RuntimeError("'{}' is not available".format(self.gallery_dir))
 
     def _process_dir(self, dir_path, relabel=False):
         img_paths = glob.glob(osp.join(dir_path, '*.jpg'))
@@ -87,5 +98,96 @@ class Market1501(BaseDataset):
             camid -= 1  # index starts from 0
             if relabel: pid = pid2label[pid]
             dataset.append((img_path, pid, camid))
+
         return dataset
 
+
+class Market1501Attributes(BaseImageDataset):
+    """
+    Market1501
+    Reference:
+    Zheng et al. Scalable Person Re-identification: A Benchmark. ICCV 2015.
+    URL: http://www.liangzheng.org/Project/project_reid.html
+
+    Dataset statistics:
+    # identities: 1501 (+1 for background)
+    # images: 12936 (train) + 3368 (query) + 15913 (gallery)
+    """
+    dataset_dir = 'market1501'
+
+    def __init__(self, root='/home/haoluo/data', verbose=True, **kwargs):
+        super(Market1501Attributes, self).__init__()
+        self.dataset_dir = osp.join(root, self.dataset_dir)
+        self.train_dir = osp.join(self.dataset_dir, 'bounding_box_train')
+        self.query_dir = osp.join(self.dataset_dir, 'query')
+        self.gallery_dir = osp.join(self.dataset_dir, 'bounding_box_test')
+        self.attribute_dir = osp.join(self.dataset_dir, 'attributes')
+        self.attributes_path = osp.join(self.attribute_dir, 'market_attribute.mat')
+        self.test_attributes_path = osp.join(self.attribute_dir, 'gallery_market.mat')
+
+        self._check_before_run()
+
+        train_data = MatlabMatrix.loadmat(self.attributes_path)['market_attribute']['train']
+        train_labels = np.array([train_data[k] for k in sorted(train_data.keys()) if k != 'image_index']).T
+        train_idx = train_data['image_index']
+        train_labels = {int(i): l for i, l in zip(train_idx, binarize(train_labels))}
+        self.num_attributes = list(train_labels.values())[0].size
+
+        train = self._process_dir(self.train_dir, train_labels, relabel=True)
+        query = self._process_dir(self.query_dir, relabel=False)
+        gallery = self._process_dir(self.gallery_dir, relabel=False)
+
+        if verbose:
+            print("=> Market1501 Attributes loaded")
+            self.print_dataset_statistics(train, query, gallery)
+
+        self.train = train
+        self.query = query
+        self.gallery = gallery
+
+        self.num_train_pids, self.num_train_imgs, self.num_train_cams = self.get_imagedata_info(self.train)
+        self.num_query_pids, self.num_query_imgs, self.num_query_cams = self.get_imagedata_info(self.query)
+        self.num_gallery_pids, self.num_gallery_imgs, self.num_gallery_cams = self.get_imagedata_info(self.gallery)
+
+    def _check_before_run(self):
+        """Check if all files are available before going deeper"""
+        if not osp.exists(self.dataset_dir):
+            raise RuntimeError("'{}' is not available".format(self.dataset_dir))
+        if not osp.exists(self.train_dir):
+            raise RuntimeError("'{}' is not available".format(self.train_dir))
+        if not osp.exists(self.query_dir):
+            raise RuntimeError("'{}' is not available".format(self.query_dir))
+        if not osp.exists(self.gallery_dir):
+            raise RuntimeError("'{}' is not available".format(self.gallery_dir))
+        if not osp.exists(self.attribute_dir):
+            raise RuntimeError("'{}' is not available".format(self.attribute_dir))
+        if not osp.exists(self.attributes_path):
+            raise RuntimeError("'{}' is not available".format(self.attributes_path))
+        if not osp.exists(self.test_attributes_path):
+            raise RuntimeError("'{}' is not available".format(self.test_attributes_path))
+
+    def _process_dir(self, dir_path, labels=None, relabel=False):
+        img_paths = glob.glob(osp.join(dir_path, '*.jpg'))
+        pattern = re.compile(r'([-\d]+)_c(\d)')
+
+        pid_container = set()
+        for img_path in img_paths:
+            pid, _ = map(int, pattern.search(img_path).groups())
+            if pid == -1: continue  # junk images are just ignored
+            pid_container.add(pid)
+        pid2label = {pid: label for label, pid in enumerate(pid_container)}
+
+        dataset = []
+        for img_path in img_paths:
+            pid, camid = map(int, pattern.search(img_path).groups())
+            if pid == -1: continue  # junk images are just ignored
+            assert 0 <= pid <= 1501  # pid == 0 means background
+            assert 1 <= camid <= 6
+            camid -= 1  # index starts from 0
+            orig_pid = pid
+            if relabel: pid = pid2label[pid]
+            if labels is not None:
+                dataset.append((img_path, pid, camid, labels[orig_pid]))
+            else:
+                dataset.append((img_path, pid, camid))
+        return dataset
