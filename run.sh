ac84cff6a93a946d46446a19afdf61edbd2edee5
Lucas Florin <lucas.florin@iosb.fraunhofer.de>
2019-05-20 09:57:38 +0200

    implemented running in background

--- run.sh
+++ run.sh
@@ -2,7 +2,9 @@ python train_baseline.py \
     --root datasets \
     -d market1501 \
     --gpu-devices 2 \
-    --max-epoch 60 \
+    --max-epoch 120 \
     --eval-split test \
-    --save-experiment=/net/merkur/storage/deeplearning/users/floluc/test_training_market/ \
+    --save-experiment=/net/merkur/storage/deeplearning/users/floluc/baseline_market/ \
+    --lr-scheduler single_step \
+    --fixbase-epoch 60
 
