bc21a9800c06c6dd56cbc4dbdd686a2694ac7932
Lucas Florin <lucas.florin@iosb.fraunhofer.de>
2019-06-25 16:34:22 +0200

    removed reduction from SCEL.

--- training/losses/sigmoid_cross_entropy_loss.py
+++ training/losses/sigmoid_cross_entropy_loss.py
@@ -2,18 +2,16 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-# TODO: Remove reduction.
 class SigmoidCrossEntropyLoss(nn.Module):
     """
     Sigmoid cross-entropy loss
     """
-    def __init__(self, num_classes=None, use_gpu=True, reduction='mean'):
+    def __init__(self, num_classes=None, use_gpu=True):
         super(SigmoidCrossEntropyLoss, self).__init__()
         self.num_classes = num_classes
         self.use_gpu = use_gpu
         self.loss_function = F.binary_cross_entropy_with_logits
         self.logits_function = nn.Sigmoid()
-        self.reduction = reduction
 
     def forward(self, inputs, targets, weight=None):
         """
@@ -26,7 +24,7 @@ class SigmoidCrossEntropyLoss(nn.Module):
         """
         if self.use_gpu:
             targets = targets.cuda()
-        loss = self.loss_function(inputs, targets, weight=weight, reduction=self.reduction)
+        loss = self.loss_function(inputs, targets, weight=weight)
         return loss
 
     def logits(self, inputs):
