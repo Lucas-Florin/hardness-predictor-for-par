c1160df5ed3c2748fd961f87f35cfd0d31297730
Lucas Florin <lucas.florin@iosb.fraunhofer.de>
2020-04-11 01:50:53 +0200

    shell script for HP-Loss calibration

--- run_rp_r.sh
+++ run_rp_r.sh
@@ -1,17 +1,18 @@
-taskset -c 20-24 \
+taskset -c 30-34 \
 python realistic_predictor_trainer.py \
     -d rap \
-    --hp-model resnet50_nh \
-    --gpu-devices 4 \
+    --gpu-devices 6 \
     --max-epoch 180 \
     --stepsize 60 120 \
     --eval-split test \
     --rejector-thresholds-split val \
-    --save-experiment=./storage/rp_rap/ \
+    --save-experiment=./storage/test/ \
     --loss-func deepmar \
     --no-hp-feedback \
     --train-batch-size 64 \
-    --ap-baseline=2020-03-28_16-40-49_checkpoint.pth.tar \
-    --use-deepmar-for-hp
+    --ap-baseline=../rp_rap/2020-03-28_16-40-49_checkpoint.pth.tar \
+    --use-deepmar-for-hp \
+    --hp-calib linear \
+    --f1-baseline=../baseline_rap/2020-03-27_21-35-57_checkpoint.pth.tar \
     --main-net-train-epochs 0 \
     --load-weights=../baseline_rap/2020-03-27_21-35-57_checkpoint.pth.tar
