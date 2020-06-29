dd4121044b09279ef1015bf004f54fe5aecbbf96
Lucas Florin <lucas.florin@iosb.fraunhofer.de>
2020-06-29 15:34:01 +0200

    removed untested models.

--- models/__init__.py
+++ models/__init__.py
@@ -1,62 +1,14 @@
-from __future__ import absolute_import
-
 from .resnet import *
-from .resnet_strong import *
-from .resnetmid import *
-from .resnext import *
-from .senet import *
-from .densenet import *
-from .inceptionresnetv2 import *
-from .inceptionv4 import *
-from .xception import *
-
-from .nasnet import *
-from .mobilenetv2 import *
-from .shufflenet import *
-from .squeezenet import *
-
-from .mudeep import *
-from .hacnn import *
-from .pcb import *
-from .mlfn import *
 from .HP_Net import *
 
 
 # TODO: Rely more on library definitions.
-# TODO: Remove untested models.
 
 
 __model_factory = {
     # image classification models
-    'resnet50': resnet50,
-    'resnet50_strong': ResNet50Strong,
-    'resnet50_fc512': resnet50_fc512,
-    'resnext50_32x4d': resnext50_32x4d,
-    'resnext50_32x4d_fc512': resnext50_32x4d_fc512,
-    'se_resnet50': se_resnet50,
-    'se_resnet50_fc512': se_resnet50_fc512,
-    'se_resnet101': se_resnet101,
-    'se_resnext50_32x4d': se_resnext50_32x4d,
-    'se_resnext101_32x4d': se_resnext101_32x4d,
-    'densenet121': densenet121,
-    'densenet121_fc512': densenet121_fc512,
-    'inceptionresnetv2': InceptionResNetV2,
-    'inceptionv4': inceptionv4,
-    'xception': xception,
-    # lightweight models
-    'nasnsetmobile': nasnetamobile,
-    'mobilenetv2': MobileNetV2,
-    'shufflenet': shufflenet,
-    'squeezenet1_0': squeezenet1_0,
-    'squeezenet1_0_fc512': squeezenet1_0_fc512,
-    'squeezenet1_1': squeezenet1_1,
-    # reid-specific models
-    'mudeep': MuDeep,
-    'resnet50mid': resnet50mid,
-    'hacnn': HACNN,
-    'pcb_p6': pcb_p6,
-    'pcb_p4': pcb_p4,
-    'mlfn': mlfn,
+    'resnet50': ResNet50,
+
     'hp_net_resnet50': HP_RES50,
     'hp_net_resnet50_nh': HP_RES50_nh,
     'hp_net_squeezenet1_0': HP_Squeezenet,
